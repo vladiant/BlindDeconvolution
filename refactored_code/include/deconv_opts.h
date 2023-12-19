85e7887e97d313811795135b447bc1ec23d0931f
Vladislav Antonov <vladislav.antonov@gmail.com>
2023-12-19 21:14:16 +0200

    Refactor options

--- refactored_code/include/deconv_opts.h
+++ refactored_code/include/deconv_opts.h
@@ -12,69 +12,67 @@
 /// fn = 'mukta.jpg'; kernel_size = 27; use_ycbcr = 0; min_lambda = 200;
 /// fn = 'fishes.jpg'; kernel_size = 31; (From Cho/Lee et. al. SIGGRAPH Asia 2009)
 /// fn = 'Levin09_im08_filt04_blurry.tif'; kernel_size = 31;
-class BlindDeblurOptions {
-public:
-
+struct BlindDeblurOptions {
 	/// set kernel_est_win to be the window used for estimating the kernel - if
 	/// this option is empty, whole image will be used
 	cv::Rect kernel_est_win;
 
 	/// set initial downsampling size for really large images
-	float prescale;
+	float prescale = 1.0;
 
 	/// This is the weight on the likelihood term - it should be decreased for
 	/// noisier images; decreasing it usually makes the kernel "fatter";
 	/// increasing makes the kernel "thinner".
-	float min_lambda;
+	float min_lambda = 100.0;
 
 	/// weight of the likelihood term
-	mutable float lambda;
+	mutable float lambda = 0.0;
 
 	/// Kernel regularization weight
-	float k_reg_wt;
+	float k_reg_wt = 0.0;
 
 	/// set this to 1 for no gamma correction - default 1.0
-	float gamma_correct;
+	float gamma_correct = 1.0;
 
 	/// threshold on fine scale kernel elements
-	float k_thresh;
+	float k_thresh = 0.0;
 
 	/// kernel initialiazation at coarsest level
 	/// 0 = uniform; 1 = vertical bar; 2 = horizontal bar; 3 = tiny 2-pixel
 	/// wide kernel at coarsest level
-	int kernel_init;
+	int kernel_init = 3;
 
 	/// delta step size for ISTA updates; increasing this delta size is not a
 	/// good idea since it may cause divergence. On the other hand decreasing
 	/// it too much will make convergence much slower.
-	mutable float delta;
+	mutable float delta = 0.001;
 
 	/// inner iterations for x estimation
-	int x_in_iter;
+	int x_in_iter = 2;
 
 	/// outer iterations for x estimation
-	int x_out_iter;
+	int x_out_iter = 2;
 
 	/// maximum number of x/k alternations per level; this is a trade-off
 	/// between performance and quality.
-	int xk_iter;
+	int xk_iter = 21;
 
 	/// non-blind settings
 	/// Maximal resudual tolerance for Bregman solution
-	float nb_lambda;
-	/// Maximal iterations for Bregman solution 
-	float nb_alpha;
-	/// Flag to use YCbCr color space
-	int use_ycbcr;
+	float nb_lambda = 3000;
 
-	int kernel_size;
+	/// Maximal iterations for Bregman solution 
+	float nb_alpha = 1.0;
+
+	/// Flag to use YCbCr color space
+	int use_ycbcr = 1;
+
+	int kernel_size = 31;
 
 	/// Maximal resudual tolerance for CG solution
-	mutable float pcg_tol;
+	mutable float pcg_tol=1e-4;
 
 	/// Maximal iterations for CG solution 
-	mutable int pcg_its;
-
-	BlindDeblurOptions();
+	mutable int pcg_its = 1;
 };
 
--- refactored_code/src/deconv_opts.cpp
+++ refactored_code/src/deconv_opts.cpp
@@ -1,30 +1,4 @@
-/*
- * deconv_opts.cpp
- *
- *  Created on: Jan 9, 2014
- *      Author: vladiant
- */
-
 #include "deconv_opts.h"
 
-BlindDeblurOptions::BlindDeblurOptions() {
-	prescale = 1.0;
-	min_lambda = 100.0;
-	k_reg_wt = 0.0;
-	lambda = 0.0;
-	gamma_correct = 1.0;
-	k_thresh = 0.0;
-	kernel_init = 3;
-	delta = 0.001;
-	x_in_iter = 2;
-	x_out_iter = 2;
-	xk_iter = 21;
-	nb_lambda = 3000;
-	nb_alpha = 1.0;
-	use_ycbcr = 1;
-	kernel_size = 31;
-	pcg_tol=1e-4;
-	pcg_its = 1;
-}
 
 
